88ccc354e4199ffd826281eff028cd912fb34de6
yogurts <scodield838727836@vip.qq.com>
2019-01-14 15:17:14 +0800

    YGMap添加注释便于理解

--- src/com/yogurts/map/YGMap.java
+++ src/com/yogurts/map/YGMap.java
@@ -38,21 +38,26 @@ public class YGMap {
     /* 添加键值对 */
     public void put(Object key, Object value) {
         YGEntry entry = new YGEntry(key, value);
-
+        // 根据哈希值得到index
         int index = key.hashCode() % arr.length;
+        // 该index对应的值为空
         if (arr[index] == null) {
+            // 新建链表，将键值对添加进链表
             YGLinkedList linkedList = new YGLinkedList();
             linkedList.add(entry);
             arr[index] = linkedList;
         } else {
+            // 该index对应的值不为空
             YGLinkedList linkedList = arr[index];
             for (int i = 0; i < linkedList.size(); ++i) {
                 YGEntry oldEntry = (YGEntry) linkedList.get(i);
+                // key已经存在，覆盖value
                 if (oldEntry.getKey().equals(key)) {
                     oldEntry.setValue(value);
                     return;
                 }
             }
+            // key不存在，添加数据
             arr[index].add(entry);
         }
     }
